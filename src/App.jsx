1fdb6f2a3beecbdf4f088736858acb2a70486e5f
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2024-05-18 19:40:20 +0530

    add quizReducer function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
+import { useEffect, useReducer } from 'react';
 import Header from './components/Header';
 import { Card } from './components/ui/card';
 import Main from './Layout/Main';
 
-function App(props) {
+const initailState = {
+  questions: [],
+  state: 'loading',
+};
+
+function quizReducer(state, action) {
+  switch (action.type) {
+    case 'getData': {
+      return {
+        ...state,
+        questions: action.quiz,
+      };
+    }
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(quizReducer, initailState);
+
+  useEffect(function () {
+    async function getQuestion() {
+      const res = await fetch('http://localhost:8000/questions');
+      const data = await res.json();
+
+      dispatch({
+        type: 'getData',
+        quiz: data,
+      });
+    }
+    getQuestion();
+  }, []);
+
   return (
     <div className='flex flex-col justify-items-center'>
       <Header />
