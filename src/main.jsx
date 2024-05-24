37f5240cd6fa3e312db6105472f142170b19673c
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2024-05-24 13:38:32 +0530

    add quizprovider to the main

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx';
 import './index.css';
 
 import ThemeProvider from './components/ui/ThemeProvider.jsx';
+import { QuizProvider } from './context/QuizContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme='dark'>
-      <App />
-    </ThemeProvider>
+    <QuizProvider>
+      <ThemeProvider defaultTheme='dark'>
+        <App />
+      </ThemeProvider>
+    </QuizProvider>
   </React.StrictMode>
 );
